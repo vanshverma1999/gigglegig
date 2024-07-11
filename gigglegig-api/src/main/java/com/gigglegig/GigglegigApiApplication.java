4e0d2bedc17192cec24d6cc47f0ece5a94cc6fe6
Vansh Verma <vansh.v@consultadd.com>
2024-07-11 08:47:55 +0530

    Initial Backend Commit

---
+++ gigglegig-api/src/main/java/com/gigglegig/GigglegigApiApplication.java
@@ -0,0 +1,13 @@
+package com.gigglegig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigglegigApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigglegigApiApplication.class, args);
+	}
+
+}
---
+++ gigglegig-api/src/test/java/com/gigglegig/GigglegigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigglegig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigglegigApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
